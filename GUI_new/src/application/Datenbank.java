3501e8090a04968ac32dfb950c18cf8bd6cf1d54
haukewilts <hauke.wilts93@googlemail.com>
2017-03-28 21:10:27 +0200

    auskommentieren der Datenbank class

--- GUI_new/src/application/Datenbank.java
+++ GUI_new/src/application/Datenbank.java
@@ -1,41 +1,78 @@
-package application;
+package application; // default package application
 
+//MySQL Library for connection Statements
+import java.sql.Connection; 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+//MySQL Library with drivers to get a connection to a MySQL Database
+import java.sql.DriverManager; 
+
+//MySQL Library for Strings with SQL commands
+import java.sql.PreparedStatement; 
+
+//MySql Library to give all information from a table
 import java.sql.ResultSet;
-import java.util.ArrayList;
 
+//MySQL Library to give out the Table as Array List
+import java.util.ArrayList; 
 
+//start with the Database class, 4 methods 
 public class Datenbank {
 
 	
-	public static ArrayList<String> get() throws Exception {
-		try {
-			Connection con = getConnection();
-			PreparedStatement statement = con.prepareStatement("SELECT username, password FROM logindaten");
+	//method that connect to the MySQL database called "schachbenutzer"
+	public static Connection getConnection() throws Exception {
 
-			ResultSet result = statement.executeQuery();
+			try {
+				//loads the java MySQL connector driver
+				String driver = "com.mysql.jdbc.Driver";
+				
+				//String that give the path where the MySQL database is saved
+				String url = "jdbc:mysql://localhost/schachbenutzer";
+				
+				//String that gives the username of the database to get access
+				String username = "root";
+				
+				//String that gives the password of the database to get access
+				String password = "";
+				
+				Class.forName(driver);
 
-			ArrayList<String> array = new ArrayList<String>();
-			while (result.next()) {
-				System.out.print(result.getString("username"));
-				System.out.print(" ");
-				System.out.println(result.getString("password"));
-
-				array.add(result.getString("password"));
+				//try to connect to the database with the information (url, username, password)
+				Connection conn = DriverManager.getConnection(url, username, password);
+				
+				//prints out that the programm is connected to the database
+				System.out.println("Datenbank Verbunden");
+				
+				//returns the url, username and password
+				return conn;
+				
+				//catch if there is anything wrong
+			} catch (Exception e) {
+				
+				//calls out what the MySQL database error is
+				System.out.println(e);
 			}
-			System.out.println("Alle Inhalte wurden wiedergegeben");
-			return array;
-
-		} catch (Exception e) {
-			System.out.println(e);
+			//return nothing if we got an error
+			return null;
 		}
-		return null;
 
-	}
+	
+	//method thats create a table into the database, called "logindaten" but only if not exist
+	public static void createTable() throws Exception {
+			try {
+				Connection con = getConnection();
+				PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS logindaten(id int NOT NULL AUTO_INCREMENT, username varchar(255), password varchar(255), PRIMARY KEY (id))");
+				create.executeUpdate();
 
+			} catch (Exception e) {
+				System.out.println(e);
+			} finally {
+				System.out.println("Tabelle erstellt");
+			}
+		}
+	
+	
+	//method that gets the entered username and password from the "regis.Controller" class to push them into the "logindaten" table
 	public static void post(String benutzer, String passwort) throws Exception{
 		final String var1 = benutzer;
 		final String var2 = passwort;
@@ -53,36 +90,50 @@ public class Datenbank {
 		}
 	}
 
-	public static void createTable() throws Exception {
-		try {
-			Connection con = getConnection();
-			PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS logindaten(id int NOT NULL AUTO_INCREMENT, username varchar(255), password varchar(255), PRIMARY KEY (id))");
-			create.executeUpdate();
+	
+	//method that gives out the stored information from the table "logindaten" from the database "schachbenutzer"
+	public static ArrayList<String> get() throws Exception {
+			try {
+				//call the method getConnection() to make sure that we are connected to the database
+				Connection con = getConnection();
+				
+				//a prepared statement that allows us to use MySQL commands as a String, here we use a Select Statement to get username and password from the table logindaten
+				PreparedStatement statement = con.prepareStatement("SELECT username, password FROM logindaten");
 
-		} catch (Exception e) {
-			System.out.println(e);
-		} finally {
-			System.out.println("Tabelle erstellt");
+				//gives out results as long as there are all given out
+				ResultSet result = statement.executeQuery();
+
+				//creates a new String Array for username and password
+				ArrayList<String> array = new ArrayList<String>();
+				
+				//as long as we have results give out the next results
+				while (result.next()) {
+					
+					//prints out username
+					System.out.print(result.getString("username"));
+					
+					//prints out a space between username and password
+					System.out.print(" ");
+					
+					//prints out password
+					System.out.println(result.getString("password"));
+
+					array.add(result.getString("password"));
+				}
+				//prints out that all information are repeated if all information are repeated
+				System.out.println("Alle Inhalte wurden wiedergegeben");
+				
+				//return the array list with username and passwords
+				return array;
+
+			//if something goes wrong he get the errors from the MySQL database
+			} catch (Exception e) {
+				
+				//prints out the error message from the MySQL database
+				System.out.println(e);
+			}
+			//the method return nothing if we have an error
+			return null;
+
 		}
-	}
-
-	public static Connection getConnection() throws Exception {
-
-		try {
-			String driver = "com.mysql.jdbc.Driver";
-			String url = "jdbc:mysql://localhost/schachbenutzer";
-			String username = "root";
-			String password = "";
-			Class.forName(driver);
-
-			Connection conn = DriverManager.getConnection(url, username, password);
-			System.out.println("Datenbank Verbunden");
-			return conn;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-		return null;
-	}
-
 }
